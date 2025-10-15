2479a897e21410af3a7e1a87b100973016c7d295
PENTAM KEERTHAN <23eg106b48@anurag.edu.in>
2025-10-15 19:19:09 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -3,15 +3,41 @@ const mongoose = require('mongoose');
 
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const options = {
+  serverSelectionTimeoutMS: 60000,
+  socketTimeoutMS: 90000,
+  maxPoolSize: 10,
+  minPoolSize: 1,
+  connectTimeoutMS: 60000,
+  family: 4,
+  ssl: true,
+  authSource: 'admin',
+  replicaSet: 'atlas-d60q75-shard-0'
+};
+
+const connectWithRetry = async () => {
+  try {
+    await mongoose.connect(mongoURI, options);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    console.log('Retrying connection in 5 seconds...');
+    setTimeout(connectWithRetry, 5000);
+  }
+};
+
+// Initial connection
+connectWithRetry();
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
+
+db.on('error', (err) => {
+  console.error('MongoDB error:', err);
+});
+
+db.on('disconnected', () => {
+  console.log('MongoDB disconnected. Attempting to reconnect...');
+  setTimeout(connectWithRetry, 5000);
 });
 
 module.exports = mongoose;
